47c9612225e395b7d58d87a3171788f713bfa45b
Furkan Doğan <1furkandogan1@gmail.com>
2023-01-24 22:12:48 +0300

    Store and TodosSlice added

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import { AiOutlineCheck,AiFillDelete } from 'react-icons/ai';
+import React from "react";
+import { AiOutlineCheck, AiFillDelete } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 export default function List() {
+  const items = useSelector((state) => state.todos.items);
+  console.log(items);
   return (
     <div id="list">
-        <div className="listItem">
-            <button className="tmm"><AiOutlineCheck/></button>
-            <div className="not">Bu benim notum</div>
-            <button className="sil"><AiFillDelete/></button>
-        </div>
-        <div className="listItem">
-            <button className="tmm"><AiOutlineCheck/></button>
-            <div className="not">Bu not</div>
-            <button className="sil"><AiFillDelete/></button>
-        </div>
-        <div className="listItem">
-            <button className="tmm"><AiOutlineCheck/></button>
-            <div className="not">Noooot</div>
-            <button className="sil"><AiFillDelete/></button>
-        </div>
+      <div className="listItem">
+        <button className="tmm">
+          <AiOutlineCheck />
+        </button>
+        <div className="not">Bu benim notum</div>
+        <button className="sil">
+          <AiFillDelete />
+        </button>
+      </div>
+      {items.map((item, key) => {
+        return (
+          <div className="listItem" key={key}>
+            <button className="tmm">
+              <AiOutlineCheck />
+            </button>
+            <div className="not">{item.title}</div>
+            <button className="sil">
+              <AiFillDelete />
+            </button>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <IconContext.Provider value={{ style: {verticalAlign: 'middle'} }}>
-      <App />
+    <IconContext.Provider value={{ style: { verticalAlign: "middle" } }}>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </IconContext.Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
